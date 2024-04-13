5a730044eb26c012d91402609f90e72089ec6f48
Koval Anastasiia <ilovepokyshat@gmail.com>
2024-04-14 00:30:18 +0300

    feat: implement getAllOrNothing func

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -122,8 +122,21 @@ function getFirstPromiseResult(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)] => Promise rejected with 2
  */
-function getAllOrNothing(/* promises */) {
-  throw new Error('Not implemented');
+function getAllOrNothing(promises) {
+  return new Promise((resolve, reject) => {
+    const resArray = [];
+    for (let i = 0; i < promises.length; i += 1) {
+      promises[i].then(
+        (value) => {
+          resArray.push(value);
+        },
+        (err) => {
+          reject(err);
+        }
+      );
+    }
+    resolve(resArray);
+  });
 }
 
 /**
